31513f68b49afc6b9cf7abc24329dfde0fdaa870
EmanAbdallah1 <emanrezk822@gmail.com>
2021-01-10 00:07:15 +0200

    eman

--- backend/assets/js/main.js
+++ backend/assets/js/main.js
@@ -1,3 +1,23 @@
 $(document).ready(function () {
     $(".select2").select2();
 })
+$(document).ready(function () {
+    $(".collapse.show").each(function () {
+        $(this).prev(".card-header").find(".fa").addClass("fa-minus").removeClass("fa-plus");
+    });
+    $(".collapse").on('show.bs.collapse', function () {
+        $(this).prev(".card-header").find(".fa").removeClass("fa-plus").addClass("fa-minus");
+    }).on('hide.bs.collapse', function () {
+        $(this).prev(".card-header").find(".fa").removeClass("fa-minus").addClass("fa-plus");
+    });
+});
+$('#file-upload').change(function () {
+    var i = $(this).prev('label').clone();
+    var file = $('#file-upload')[0].files[0].name;
+    $(this).prev('label').text(file);
+});
+$('#upload').change(function () {
+    var i = $(this).prev('label').clone();
+    var file = $('#upload')[0].files[0].name;
+    $(this).prev('label').text(file);
+});
